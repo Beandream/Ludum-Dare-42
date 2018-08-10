2334499398bb903fe3c97c8aab3671eceada34d9
Beandream <beandreamed@gmail.com>
2018-08-10 07:55:20 -0400

    Added All the Main Elements
    added Phaser, added Html to run game, added sample assets, added game.js with basic code to make a canvas.

--- game.js
+++ game.js
@@ -1,1 +1,30 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            // debug: true,
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
 
+var game = new Phaser.Game(config);
+
+function preload(){
+
+}
+
+function create(){
+
+}
+function update(){
+    
+}
