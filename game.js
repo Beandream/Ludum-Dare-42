39b2f5fca311c00ce6b6ff4093372278423f145b
Beandream <beandreamed@gmail.com>
2018-08-11 15:33:44 -0400

    Added Sample Things

--- game.js
+++ game.js
@@ -17,14 +17,31 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+var spike1;
+var spike2;
+var speed = 3;
 
 function preload(){
-    this.load.image('test', 'assets/testImage.png');
+    this.load.image('background', 'assets/background.png');
+    this.load.image('spikeWall', 'assets/spikeWall.png');
+    this.load.image('greenSlime', 'assets/greenSlime.png');
 }
 
 function create(){
-    this.add.image(400, 300, 'test');
+    this.add.image(400, 300, 'background');
+    this.add.image(350, 300, 'greenSlime').setScale(0.2);
+    this.add.image(400, 300, 'greenSlime').setScale(0.3);
+    this.add.image(450, 300, 'greenSlime').setScale(0.4);
+    spike1 = this.physics.add.sprite(-500, 300, 'spikeWall');
+    spike2 = this.physics.add.sprite(1300, 300, 'spikeWall').setFlipX(true).setFlipY(true);
+
+    this.physics.add.overlap(spike2, spike1, End);
 }
 function update(){
+    // spike1.x += speed;
+    // spike2.x += -speed;
+}
 
+function End(){
+    speed = 0;
 }
